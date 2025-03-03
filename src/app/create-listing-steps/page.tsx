0490d9eda171c83b23cc46b179c427a1f1189e9e
Sanyam Pandey <sanyam071104@gmail.com>
2025-03-03 12:33:02 +0530

    Update page.tsx

--- src/app/create-listing-steps/page.tsx
+++ src/app/create-listing-steps/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/NavBar";
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
