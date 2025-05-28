00395f6a26a60b998849a3945ad0e366c2dc09df
Sanyam Pandey <sanyam071104@gmail.com>
2025-05-29 01:39:37 +0530

    done

--- src/app/create-listing-steps/step3.tsx
+++ src/app/create-listing-steps/step3.tsx
@@ -44,7 +44,7 @@ const LocationSelector: React.FC<LocationSelectorProps> = ({
   };
 
   return (
-    <div className="bg-white rounded-t-[2rem] h-full overflow-y-auto">
+    <div className="bg-white rounded-t-[2rem] h-screen overflow-y-hidden">
       <div className="p-6 max-w-md mx-auto w-full flex flex-col justify-between h-full">
         {/* Top Section: Location & Intersection */}
         <div className="space-y-6">
